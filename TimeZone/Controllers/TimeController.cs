7243d384920be30530d1cf8440f83b20752485ea
Ali Akpoyraz <128495051+aliakpoyraz@users.noreply.github.com>
2025-01-07 14:19:41 +0300

    TimeZone API - timezone,date,utc

---
+++ TimeZone/Controllers/TimeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+using NodaTime.Text;
+using System;
+
+namespace TimeZone.Controllers
+{
+    [Route("api/time")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly IDateTimeZoneProvider _timeZoneProvider;
+
+        public TimeController()
+        {
+            _timeZoneProvider = DateTimeZoneProviders.Tzdb;
+        }
+
+        [HttpGet("{timezone}")]
+        public IActionResult GetTime(string timezone)
+        {
+            var dateTimeZone = _timeZoneProvider.GetZoneOrNull(timezone);
+            if (dateTimeZone == null)
+            {
+                return NotFound(new { Message = $"Geçersiz zaman dilimi: {timezone}" });
+            }
+
+            var now = SystemClock.Instance.GetCurrentInstant();
+            var zonedDateTime = now.InZone(dateTimeZone);
+
+            var pattern = LocalDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
+            var formattedLocalTime = pattern.Format(zonedDateTime.LocalDateTime);
+
+            return Ok(new
+            {
+                Timezone = timezone,
+                LocalTime = formattedLocalTime,
+                UtcOffset = zonedDateTime.Offset.ToString()
+            });
+        }
+
+        [HttpGet("timezones")]
+        public IActionResult GetTimezones()
+        {
+            var timeZones = _timeZoneProvider.Ids;
+            return Ok(timeZones);
+        }   
+    }
+}
---
+++ TimeZone/Program.cs
@@ -0,0 +1,35 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() 
+              .AllowAnyMethod()  
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+builder.WebHost.UseUrls("https://localhost:7055");
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
+
+app.Run();
